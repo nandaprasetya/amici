8032cc9ba4a4f5169ee48fdf5988ac119bac6dc4
nandaprasetya <nandaprasetya712@gmail.com>
2025-09-29 22:11:34 +0800

    Feat: Role and Users Migration

--- database/migrations/2025_09_30_000000_create_users_table.php
+++ database/migrations/2025_09_30_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained('roles', 'role_id')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
